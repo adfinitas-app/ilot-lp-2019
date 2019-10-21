5c0fe5c24b2a75fcd3b22dd3edb29a00cac08e85
Maurin Liberal <mliberal@dhcp133.adfinitas.fr>
2019-10-21 16:25:23 +0200

    [~] change texts in dons

--- js/header.js
+++ js/header.js
@@ -2,11 +2,11 @@ function checkHeight() {
     var heightElement = window.scrollY;
     console.log("height of Element: " + heightElement);
     if (heightElement <= 0) {
-        document.getElementById("top-header").style.backgroundColor = "white";
-        $(".social-header").css('display', 'block');
-    } else {
         document.getElementById("top-header").style.backgroundColor = "rgba(0, 0, 0, 0)";
         $(".social-header").css('display', 'none');
+    } else {
+        document.getElementById("top-header").style.backgroundColor = "white";
+        $(".social-header").css('display', 'block');
     }
 }
 
