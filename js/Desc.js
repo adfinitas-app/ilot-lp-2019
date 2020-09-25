c8467600cce5bfd949c8d7bdd1eb6fe49a8dbbe8
Pierrick Gouerec <pgouerec@adfinitas.fr>
2020-09-25 10:45:04 +0200

    Revert "[*] Update LP info for 2019"
    This reverts commit b9a46a461a145229b85859c03bcfc422a14c4534.

--- js/Desc.js
+++ js/Desc.js
@@ -65,7 +65,7 @@ $( window ).resize(function () {
             document.getElementById("top-header").style.backgroundColor = "rgba(0,0,0,0)";
         }
         $("#TEM1").attr("src", "https://adfinitas-statics-cdn.s3.eu-west-3.amazonaws.com/lilot/Lilot-LP-2019/TEM1.png");
-        $("#TEM2").attr("src", "https://adfinitas-statics-cdn.s3.eu-west-3.amazonaws.com/lilot/Lilot-LP-2019/TEM2-new.jpg");
+        $("#TEM2").attr("src", "https://adfinitas-statics-cdn.s3.eu-west-3.amazonaws.com/lilot/Lilot-LP-2019/TEM2.png");
         $("#TEM3").attr("src", "https://adfinitas-statics-cdn.s3.eu-west-3.amazonaws.com/lilot/Lilot-LP-2019/TEM4.png");
         $("#TEM4").attr("src", "https://adfinitas-statics-cdn.s3.eu-west-3.amazonaws.com/lilot/Lilot-LP-2019/donateur_TEM.png");
 
